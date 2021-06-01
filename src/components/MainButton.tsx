ba8b55be24e2120f80b4d203fe0ad6e0fe816cf0
sbmose <superbright43@gmail.com>
2021-06-01 19:22:42 +0200

    component(MainButton)

--- src/components/MainButton.tsx
+++ src/components/MainButton.tsx
@@ -1,27 +1,51 @@
 import React from 'react';
-import { StyleSheet, TouchableOpacity, Text } from 'react-native';
+import { StyleSheet, TouchableOpacity, Text, Image, View } from 'react-native';
 import { Theme } from '../themes/default';
 
-export default function MainButton({ label, style, ...props }: any) {
+export default function MainButton(props: any) {
+    const { label, style, image, textColor, bgColor } = props;
+
     return (
         <TouchableOpacity
             activeOpacity={0.7}
-            style={[styles.button, style]}
-            {...props} >
-            <Text style={styles.buttonLabel}>{label}</Text>
+            style={[styles.button, style, bgColor && { backgroundColor: bgColor }]} >
+            {image && (
+                <View style={[styles.imageContainer]}>
+                    <Image source={image} resizeMode='contain' style={styles.buttonImage} />
+                </View>
+            )}
+            <View style={[styles.textContainer,]}>
+                <Text style={[styles.buttonLabel, textColor && { color: textColor }]}>{label}</Text>
+            </View>
         </TouchableOpacity>
     );
 }
 
 const styles = StyleSheet.create({
     button: {
-        flexDirection: "column",
+        flexDirection: "row",
         alignItems: "center",
-        justifyContent: "center",
+        justifyContent: "flex-start",
         height: 56,
         backgroundColor: Theme.pink,
         borderRadius: 6,
-        marginHorizontal: 16
+        marginHorizontal: 0,
+        paddingHorizontal: 16
+    },
+    imageContainer: {
+        flexDirection: "column",
+        alignItems: "center",
+        marginRight: 16
+    },
+    textContainer: {
+        flex: 1,
+        flexDirection: "column",
+        alignItems: "center"
+    },
+    buttonImage: {
+        width: 25,
+        height: 25,
+        alignSelf: 'flex-start'
     },
     buttonLabel: {
         fontSize: 16,
--- src/screen/Authentication/Login.screen.tsx
+++ src/screen/Authentication/Login.screen.tsx
@@ -9,11 +9,27 @@ import {
 import { Card } from "native-base";
 import { Theme, ThemeStyles } from '../../themes/default';
 import FloatingInput from '../../components/FloatingInput';
+import MainButton from '../../components/MainButton';
 
 // Screen Styles
 
 export default function LoginScreen({ navigation }: any) {
 
+    const handleEmailLogin = () => {
+        console.log("handleEmailLogin");
+    }
+
+    const handleFacebookLogin = () => {
+        console.log("handleFacebookLogin");
+    }
+
+    const handleGoogleLogin = () => {
+        console.log("handleGoogleLogin");
+    }
+
+    const handleCreateAccount = () => {
+        console.log("handleCreateAccount");
+    }
 
     return (
         <SafeAreaView style={ThemeStyles.safeAreaContainer}>
@@ -28,6 +44,24 @@ export default function LoginScreen({ navigation }: any) {
                             label="Heslo"
                             style={styles.input}
                             isPassword />
+                        <MainButton
+                            label="Pokračovať"
+                            style={styles.buttonContainer}
+                            onPress={() => handleEmailLogin()} />
+                        <MainButton
+                            label="Prihlásiť sa cez Google účet"
+                            style={styles.buttonContainer}
+                            bgColor={Theme.white}
+                            textColor={Theme.black}
+                            image={require('../../../assets/google.png')}
+                            onPress={() => handleGoogleLogin()} />
+                        <MainButton
+                            label="Prihlásiť sa cez Facebook"
+                            style={styles.buttonContainer}
+                            bgColor={Theme.blue}
+                            textColor={Theme.white}
+                            image={require('../../../assets/facebook.png')}
+                            onPress={() => handleFacebookLogin()} />
                     </View>
                 </View>
             </ScrollView>
@@ -52,6 +86,9 @@ const styles = StyleSheet.create({
         marginBottom: 30
     },
     input: {
+        marginBottom: 16
+    },
+    buttonContainer: {
         marginBottom: 16
     }
 });
